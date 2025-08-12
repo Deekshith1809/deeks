1e483876ec33f8c18b6437e965685cf22bf22419
Deekshith1809 <149555762+Deekshith1809@users.noreply.github.com>
2025-08-12 12:31:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/s207">
+     <Router basename="/skill3_gh-pages/">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
